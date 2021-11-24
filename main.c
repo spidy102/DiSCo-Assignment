06def31fe4a8302df06e7c9f1aaa81e215a3497d
spidy102 <shauryamarwah02@gmail.com>
2021-11-24 18:03:52 +0530

    updates -- menu1

--- main.c
+++ main.c
@@ -1,6 +1,122 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+void menu4()
+{
+    printf("1. Display the hasse diagram\n");
+    printf("2. Display the website whose link is available in every website\n");
+    printf("3. Display the website which has links of every website\n");
+    printf("4. Display the websites that do not have links to any other website except itself\n");
+    printf("5. Display the websites which cant be reached from any other website except itself\n");
+    printf("6. Given some websites, display the websites which are rechable from all of them\n");
+    printf("7. Given some websites, display the websites from which you can reach all those websites\n");
+    printf("8. Is this relation an example of lattice?\n");
+    printf("9. Return to main menu\n");
+}
+
+int choice1(int **mat, int rows, int columns)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (mat[i][j] == 0)
+            {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int choice2(int **mat, int rows, int columns)
+{
+    for (int i = 0; i < columns; i++)
+    {
+        for (int j = i; j < rows; j++)
+        {
+            if ((mat[j][i] == 0 && mat[i][j] == 1) || (mat[j][i] == 1 && mat[i][j] == 0))
+            {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int choice3(int **mat, int rows, int columns)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (mat[i][j])
+            {
+                for (int k = 0; k < columns; k++)
+                {
+                    if (mat[j][k] && !mat[i][k])
+                    {
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+    return 1;
+}
+
+int choice4(int **mat, int rows, int columns)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (mat[i][j] == 1)
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int choice5(int **mat, int rows, int columns)
+{
+    if (choice2(mat, rows, columns))
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int choice6(int **mat, int rows, int columns)
+{
+    for (int i = 0; i < columns; i++)
+    {
+        for (int j = i + 1; j < rows; j++)
+        {
+            if ((mat[j][i] == 0 && mat[i][j] == 1) || (mat[j][i] == 1 && mat[i][j] == 0))
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int choice7(int **mat, int rows, int columns)
+{
+}
+
+int choice8(int **mat, int rows, int columns)
+{
+    return (choice1(mat, rows, columns) && choice2(mat, rows, columns) && choice3(mat, rows, columns)) ? (1) : (0);
+}
+
 int main()
 {
     char buffer[1024];
@@ -22,7 +138,12 @@ int main()
         columns++;
         tok = strtok(NULL, ",");
     }
-    int mat[rows - 1][columns - 1];
+    int **mat = malloc((rows - 1) * sizeof(int *));
+
+    for (int i = 0; i < rows - 1; i++)
+    {
+        mat[i] = malloc((columns - 1) * sizeof(int));
+    }
     fclose(fp);
     fp = fopen("SampleInput.csv", "r");
     int counter = 0;
@@ -48,6 +169,7 @@ int main()
                 }
                 else
                 {
+
                     mat[counter - 2][i - 2] = atoi(tken);
                     tken = strtok(NULL, ",");
                 }
@@ -64,17 +186,218 @@ int main()
     //     }
     //     printf("\n");
     // }
-    printf("Main Menu");
-    printf("\n");
-    printf("1. Does every website has a link to itself\n");
-    printf("2. Is it possible to always return back to the previous website from the current website in one step?\n");
-    printf("3. Does every website has all the links to the website which are reachable from it?\n");
-    printf("4. Does there exist any website that contains a link to itself?\n");
-    printf("5. Is it impossible to return to the previous website from the current website in one step?\n")
+    while (1)
+    {
+        printf("Main Menu");
+        printf("\n");
+        printf("1. Does every website has a link to itself\n");
+        printf("2. Is it possible to always return back to the previous website from the current website in one step?\n");
+        printf("3. Does every website has all the links to the website which are reachable from it?\n");
+        printf("4. Does there exist any website that contains a link to itself?\n");
+        printf("5. Is it impossible to return to the previous website from the current website in one step?\n");
         printf("6. Is it impossible to return to the previous website from the current website in one step (excluding the cases where the current and previous website are the same)\n");
-    printf("7. Is it possible to divide the network into multiple pieces such that every website in a piece is reachable from every other website in that piece?\n");
-    printf("8. Is thsi relation an example of poset?\n")
+        printf("7. Is it possible to divide the network into multiple pieces such that every website in a piece is reachable from every other website in that piece?\n");
+        printf("8. Is this relation an example of poset?\n");
         printf("9. Exit\n");
+        int choice;
+        scanf("%d", &choice);
 
+        if (choice == 1)
+        {
+            int ans = choice1(mat, rows - 1, columns - 1);
+            if (ans == 0)
+            {
+                printf("No\n");
+                printf("Do you want to visualize how the network will look if we add minimum links to satisfy this property?1 for yes, 0 for no\n");
+                int choice_;
+                scanf("%d", &choice_);
+                if (choice == 1)
+                {
+                    printf("Hello");
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                printf("Yes\n");
+                continue;
+            }
+        }
+        else if (choice == 2)
+        {
+            int ans = choice2(mat, rows - 1, columns - 1);
+            if (ans == 0)
+            {
+                printf("No\n");
+                printf("Do you want to visualize how the network will look if we add minimum links to satisfy this property?1 for yes, 0 for no\n");
+                int choice_;
+                scanf("%d", &choice_);
+                if (choice == 1)
+                {
+                    printf("Hello");
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                printf("Yes\n");
+                continue;
+            }
+        }
+        else if (choice == 3)
+        {
+            int ans = choice3(mat, rows, columns);
+            if (ans == 0)
+            {
+                printf("No\n");
+                printf("Do you want to visualize how the network will look if we add minimum links to satisfy this property?1 for yes, 0 for no\n");
+                int choice_;
+                scanf("%d", &choice_);
+                if (choice == 1)
+                {
+                    printf("Hello");
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                printf("Yes\n");
+                continue;
+            }
+        }
+        else if (choice == 4)
+        {
+            int ans = choice4(mat, rows, columns);
+            if (ans == 0)
+            {
+                printf("No\n");
+                printf("Do you want to visualize how the network will look if we add minimum links to satisfy this property?1 for yes, 0 for no\n");
+                int choice_;
+                scanf("%d", &choice_);
+                if (choice == 1)
+                {
+                    printf("Hello");
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                printf("Yes\n");
+                continue;
+            }
+        }
+        else if (choice == 5)
+        {
+            int ans = choice5(mat, rows, columns);
+            if (ans == 0)
+            {
+                printf("No\n");
+                printf("Do you want to visualize how the network will look if we add minimum links to satisfy this property?1 for yes, 0 for no\n");
+                int choice_;
+                scanf("%d", &choice_);
+                if (choice == 1)
+                {
+                    printf("Hello");
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                printf("Yes\n");
+                continue;
+            }
+        }
+        else if (choice == 6)
+        {
+            int ans = choice6(mat, rows, columns);
+            if (ans == 0)
+            {
+                printf("No\n");
+                printf("Do you want to visualize how the network will look if we add minimum links to satisfy this property?1 for yes, 0 for no\n");
+                int choice_;
+                scanf("%d", &choice_);
+                if (choice == 1)
+                {
+                    printf("Hello");
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                printf("Yes\n");
+                continue;
+            }
+        }
+        else if (choice == 7)
+        {
+            int ans = choice7(mat, rows, columns);
+            if (ans == 0)
+            {
+                printf("No\n");
+                printf("Do you want to visualize how the network will look if we add minimum links to satisfy this property?1 for yes, 0 for no\n");
+                int choice_;
+                scanf("%d", &choice_);
+                if (choice == 1)
+                {
+                    printf("Hello");
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                printf("Yes\n");
+                continue;
+            }
+        }
+        else if (choice == 8)
+        {
+            int ans = choice8(mat, rows, columns);
+            if (ans == 0)
+            {
+                printf("No\n");
+                // printf("Do you want to visualize how the network will look if we add minimum links to satisfy this property?1 for yes, 0 for no\n");
+                // int choice_;
+                // scanf("%d", &choice_);
+                // if (choice == 1)
+                // {
+                //     printf("Hello");
+                // }
+                // else
+                // {
+                //     continue;
+                // }
+            }
+            else
+            {
+                printf("Yes\n");
+                // continue;
+            }
+        }
+        else if (choice == 9)
+        {
+            break;
+        }
+    }
     return 0;
 }
--- visualiser.c
+++ visualiser.c
@@ -1,14 +1,22 @@
-#include<unistd.h>
-void plot(char * fname){
+#include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+void plot(char *fname)
+{
     int pid;
-    if((pid = fork())==0){
-        if(execlp("python", "python","visualise.py",fname,(char*)NULL)==-1){
-        	execlp("python3", "python3","visualise.py",fname,(char*)NULL);
+    if ((pid = fork()) == 0)
+    {
+        if (execlp("python", "python", "visualise.py", fname, (char *)NULL) == -1)
+        {
+            execlp("python3", "python3", "visualise.py", fname, (char *)NULL);
         };
     }
     exit(0);
 }
-int main(){
-    plot("mycsv.csv");
+int main()
+{
+    plot("SampleInput.csv");
     return 0;
 }
